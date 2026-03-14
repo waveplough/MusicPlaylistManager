a36e2d35fa76cec612ffb32417358c48414c141f
David Jacob <daviddjacob11@gmail.com>
2026-03-14 15:41:41 -0400

    Basic class structure of Song

--- include/Song.h
+++ include/Song.h
@@ -1,6 +1,17 @@
 #pragma once
 #include "PlaylistItem.h"
+#include <iostream>
 class Song : public PlaylistItem {
+private:
+	std::string artist;		// artist name
+	std::string album;		// album name
+	std::string genre;		// genre name
+	int playCount;			// number of plays
 
+public:
+	Song();							// default constructor
+	virtual ~Song();				// destructor
+	virtual void displayItem();		// outputs item
+	void incrementPlayCount();		// increments the play count
 };
 
--- src/Song.cpp
+++ src/Song.cpp
@@ -1,1 +1,15 @@
 #include "Song.h"
+
+Song::Song() : artist(""), album(""), genre(""), playCount(0) {}	// default constructor
+
+Song::~Song() {														// destructor
+
+}
+
+void Song::displayItem() {											// displays item
+
+}							
+
+void Song::incrementPlayCount() {									// increments the play count
+
+}									

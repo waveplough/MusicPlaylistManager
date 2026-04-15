432f9d39e791ca9642eefc6c8314ca1e8aecb03b
David Jacob <daviddjacob11@gmail.com>
2026-04-15 16:04:38 -0400

    Revert "Merge pull request #60 from waveplough/GUI"
    This reverts commit 9b025973a23069649648fb016e2ed73c505b8cab, reversing
    changes made to 8adc7b0e7e88df1cafdd68e6facddc31ae680e94.

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -59,8 +59,6 @@ public slots:
 
     // Analytics Page
     void onAnalyticsExitButtonClicked();
-    void updateListeningTime();
-    void updateAnalyticsDisplay();
 
     // Playlist Tab
     void addPlaylistCard();
@@ -132,9 +130,6 @@ private:
     // It is used to keep track of which songs are currently being displayed in the library list, 
     // so that when a user clicks on a song, we can get the correct song object from this vector.
     std::vector<std::shared_ptr<Song>> currentSearchResults;
-
-    QTimer* listeningTimer = nullptr;   // QTimer that keeps track of total play time
-    int totalListeningTimeSeconds = 0;  // The total time as an integer for ease of use
 
 };
 #endif // MAINWINDOW_H

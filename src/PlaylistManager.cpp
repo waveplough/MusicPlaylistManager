5b3a752e0e699fbffdb0270c938833b19e6675d9
David Jacob <daviddjacob11@gmail.com>
2026-04-18 08:50:49 -0400

    Playlist manager comments

--- src/PlaylistManager.cpp
+++ src/PlaylistManager.cpp
@@ -1,14 +1,29 @@
 #include "PlaylistManager.h"
 #include "Playlist.h"
 
+/**
+ * Initializes PlaylistManager with a reference to the MusicLibrary
+ *
+ * @param lib Shared pointer to the MusicLibrary containing all songs and playlists
+ */
 PlaylistManager::PlaylistManager(std::shared_ptr<MusicLibrary>& lib)
     : library(lib)
 {
 }
 
+/**
+ * Cleans up any resources
+ */
 PlaylistManager::~PlaylistManager() {
 }
 
+/**
+ * Adds a song to a specified playlist
+ *
+ * @param songID Unique identifier of the song to add
+ * @param playlistID Unique identifier of the target playlist
+ * @return true if the song was successfully added, false otherwise
+ */
 bool PlaylistManager::addSongToPlaylist(const std::string& songID,const std::string& playlistID) {
     std::shared_ptr<Song> targetedSong = nullptr;
 
@@ -34,6 +49,13 @@ bool PlaylistManager::addSongToPlaylist(const std::string& songID,const std::str
     return false;
 }
 
+/**
+ * Removes a song from a specified playlist
+ *
+ * @param songID Unique identifier of the song to remove
+ * @param playlistID Unique identifier of the playlist to remove from
+ * @return true if the playlist was found and removal attempted, false otherwise
+ */
 bool PlaylistManager::removeSongFromPlayList(const std::string& songID,const std::string& playlistID) {
     for (const auto& p : library->getPlaylists()) {
         if (p->getPlaylistID() == playlistID) {
@@ -44,6 +66,11 @@ bool PlaylistManager::removeSongFromPlayList(const std::string& songID,const std
     return false;
 }
 
+/**
+ * Displays statistics for a specific song
+ *
+ * @param songID Unique identifier of the song to query
+ */
 void PlaylistManager::getSongStats(const std::string& songID) const {
     for (const auto& s : library->getSongs()) {
         if (s->getItemID() == songID) {
